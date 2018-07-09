41a475b67bf16e5040c3a707f2997e8b428efd2d
郭腾飞 <12345678>
2018-07-09 11:22:40 +0800

    提交文件

---
+++ src/main/java/com/seda/springboot/SpringbootApplication.java
@@ -0,0 +1,20 @@
+package com.seda.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringbootApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringbootApplication.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
